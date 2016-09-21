716d49a83ab0ddd47e78612f27d9473c38a73b70
Jorge Ulises Useche Cuellar <naturalmentejorge@gmail.com>
2016-09-20 19:50:50 -0500

    Se agregan pruebas unitarias

--- README.md
+++ README.md
@@ -1,6 +1,21 @@
 # jwt-beego
 Una implementación simple de dgrijalva/jwt-go para beego. 
 
+```go
+func (c *TipoCancelacionSemestreController) Prepare() {
+	tokenString := c.Ctx.Input.Query("tokenString")
+
+	et := jwtbeego.EasyToken{}
+	valido, _ := et.ValidateToken(tokenString)
+	if !valido {
+		c.Ctx.Output.SetStatus(401)
+		c.Data["json"] = "Permission Deny"
+		c.ServeJSON()
+	}
+	return
+}
+```
+
 Se basa en:
-https://github.com/someone1/gcp-jwt-go
-https://github.com/dgrijalva/jwt-go
+* https://github.com/someone1/gcp-jwt-go
+* https://github.com/dgrijalva/jwt-go
--- jwt_test.go
+++ jwt_test.go
@@ -7,16 +7,34 @@ import (
 	"github.com/juusechec/jwt-beego"
 )
 
+var (
+	tokenStringGlobal string
+)
+
 func TestGetToken(t *testing.T) {
 	et := jwtbeego.EasyToken{
 		Username: "username",
 		Expires:  time.Now().Unix() + 3600, //Segundos
 	}
 	tokenString, err := et.GetToken()
+	tokenStringGlobal = tokenString
 	if tokenString == "" {
 		t.Errorf("Token String empty.")
 	}
 	if err != nil {
 		t.Errorf("Error while verifying GetToken: %v", err)
 	}
+}
+
+func TestValidateToken(t *testing.T) {
+	tokenString := tokenStringGlobal //c.Ctx.Input.Query("username")
+
+	et := jwtbeego.EasyToken{}
+	valido, err := et.ValidateToken(tokenString)
+
+	if !valido {
+		t.Errorf(err.Error())
+	}
+
+	return
 }
